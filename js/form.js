bc670daa089531f1eefef23a8de2a0244f59d5c3
jesusvgessa <jesusvgessa@hotmail.com>
2022-01-15 00:25:18 +0100

    formulario

--- js/form.js
+++ js/form.js
@@ -1,30 +1,70 @@
 window.onload = load;
 
-function load(){
+function load() {
     //Variables:
     var formJug1 = document.getElementById("formJug1");
     var btnJug1 = document.getElementById("btnJug1");
+    var errorJug1 = document.getElementById("errorJug1");
 
-
-    var avatar1 = document.getElementById("avatarJug1");
+    var formAvatar1 = document.getElementById("avatarJug1");
     var btnAva1 = document.getElementById("btnAva1");
+    var errorAva1 = document.getElementById("errorAva1");
 
-    var nombreJug2 = document.getElementById("nombreJug2");
     var formJug2 = document.getElementById("formJug2");
     var btnJug2 = document.getElementById("btnJug2");
-    var error = document.getElementById("errores");
+    var errorJug2 = document.getElementById("errorJug2");
 
+    var formAvatar2 = document.getElementById("avatarJug2");
+    var btnAva2 = document.getElementById("btnAva2");
+    var errorAva2 = document.getElementById("errorAva2");
 
     //Añado los eventos convenientes:
-    btnJug1.addEventListener("click", validar);
+    btnJug1.addEventListener("click", validarNombre1);
+    btnAva1.addEventListener("click", validarAvatar1);
+    btnJug2.addEventListener("click", validarNombre2);
+    btnAva2.addEventListener("click", validarAvatar2);
 
-    function validar(event) {
+    function validarNombre1(event) {
         event.preventDefault();
         if (document.getElementById("nombreJug1").value != "") {
+            if (errorJug1.innerText.length != 0) {
+                errorJug1.removeChild(errorJug1.firstChild);
+            } //Fin Si
             var jugador1 = document.getElementById("nombreJug1").value;
-            avatar1.style.display = "block";
+            formAvatar1.style.display = "block";
         } else {
-            error.innerHTML = "ERROR: Introduce un nombre.";
-        }
+            errorJug1.innerText = "ERROR: Introduce un nombre.";
+        } //Fin Si
+    } //Fin Funcion
+
+    function validarAvatar1(event) {
+        event.preventDefault();
+        var avatar1 = document.getElementById("avatar1").value;
+        formJug1.style.display = "none";
+        formAvatar1.style.display = "none";
+        formJug2.style.display = "block";
+    }
+
+    function validarNombre2(event) {
+        event.preventDefault();
+        if (document.getElementById("nombreJug2").value != "") {
+            if (errorJug2.innerText.length != 0) {
+                errorJug2.removeChild(errorJug2.firstChild);
+            } //Fin Si
+            var jugador2 = document.getElementById("nombreJug2").value;
+
+            //Recorro la lista de avatares del 2 jugador, para borrar el que ya esta elegido
+            var lista = document.querySelectorAll(".listaAvatares2>li");
+            console.log(lista[0].attributes.src);
+            formAvatar2.style.display = "block";
+
+        } else {
+            errorJug2.innerText = "ERROR: Introduce un nombre.";
+        } //Fin Si
+    } //Fin Funcion
+
+    function validarAvatar2(event) {
+        event.preventDefault();
+        var avatar2 = document.getElementById("avatar2").value;
     }
 }
--- js/galeria.js
+++ js/galeria.js
@@ -7,7 +7,7 @@ var lista = document.querySelectorAll("li>img");
 //Les doy un evendo onClick a todas
 for (var i = 0; i < lista.length; i++) {
     lista[i].addEventListener("click", cambioPrincipal);
-}//Fin Para
+} //Fin Para
 
 //Funcion para cambiar la imagen principal
 function cambioPrincipal() {
@@ -19,41 +19,40 @@ function cambioPrincipal() {
 var filtro = document.getElementById("filtroTema");
 filtro.addEventListener("change", filtroTematica);
 
-function filtroTematica(){
+function filtroTematica() {
     //Cojo el valor del select
     var tematica = document.getElementById("filtroTema").value;
     var principal = document.getElementById("principal");
     var fondo = document.getElementById("cuadroPrincipal");
 
     //Depende del valor del select, le cambio la url a todas las fotos y el fondo
-    switch (tematica){
+    switch (tematica) {
         case "gato":
             //Cambio las fotos de la lista
-            for(var i=0;i < lista.length; i++){
-                lista[i].setAttribute("src", "../img/gato/foto"+(i+1)+".png");
-            }//Fin Para
+            for (var i = 0; i < lista.length; i++) {
+                lista[i].setAttribute("src", "../img/gato/foto" + (i + 1) + ".png");
+            } //Fin Para
 
             //Cambio tanto el fondo de la imagen principal, como la imagen
             //dejando la primera por defecto
-            principal.setAttribute("src" ,"../img/gato/foto1.png");
+            principal.setAttribute("src", "../img/gato/foto1.png");
             fondo.style.backgroundImage = "url('../img/gato/fondo.jpg')";
             break;
-        
+
         case "futbol":
-            for(var i=0;i < lista.length; i++){
-                lista[i].setAttribute("src", "../img/futbol/foto"+(i+1)+".png");
-            }//Fin Para
-            principal.setAttribute("src" ,"../img/futbol/foto1.png");
+            for (var i = 0; i < lista.length; i++) {
+                lista[i].setAttribute("src", "../img/futbol/foto" + (i + 1) + ".png");
+            } //Fin Para
+            principal.setAttribute("src", "../img/futbol/foto1.png");
             fondo.style.backgroundImage = "url('../img/futbol/fondo.jpg')";
             break;
-        
+
         case "ovni":
-            for(var i=0;i < lista.length; i++){
-                console.log(lista[i].attributes.src);
-                lista[i].setAttribute("src", "../img/ovni/foto"+(i+1)+".png");
-            }//Fin Para
-            principal.setAttribute("src" ,"../img/ovni/foto1.png");
+            for (var i = 0; i < lista.length; i++) {
+                lista[i].setAttribute("src", "../img/ovni/foto" + (i + 1) + ".png");
+            } //Fin Para
+            principal.setAttribute("src", "../img/ovni/foto1.png");
             fondo.style.backgroundImage = "url('../img/ovni/fondo.jpg')";
             break;
-    }//Fin Segin Sea
+    } //Fin Segin Sea
 }
